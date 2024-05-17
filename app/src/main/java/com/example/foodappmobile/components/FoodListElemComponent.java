ebe306d4f6fd4f2e6dcb0ee59abfd49b66da6a6f
Krzysztof <kprzeslawski01@gmail.com>
2024-05-17 22:43:10 +0200

    successful draw of new component

--- app/src/main/java/com/example/foodappmobile/components/FoodListElemComponent.java
+++ app/src/main/java/com/example/foodappmobile/components/FoodListElemComponent.java
@@ -5,95 +5,51 @@ import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
-import android.graphics.drawable.Drawable;
 import android.text.TextPaint;
 import android.util.AttributeSet;
-import android.view.View;
+import android.view.LayoutInflater;
 import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import com.example.foodappmobile.R;
 
-/**
- * TODO: document your custom view class.
- */
-public class FoodListElemComponent extends View {
-    private String mExampleString; // TODO: use a default from R.string...
-    private int mExampleColor = Color.RED; // TODO: use a default from R.color...
-    private float mExampleDimension = 0; // TODO: use a default from R.dimen...
-
-    private TextPaint mTextPaint;
-    private Button button;
-    private float mTextWidth;
-    private float mTextHeight;
+public class FoodListElemComponent extends ConstraintLayout {
+    private TextView countValue;
+    private Button buttonAddValue;
+    private Button buttonDecreaseValue;
 
     public FoodListElemComponent(Context context) {
         super(context);
-        init(null, 0);
+        init(context,null, 0);
     }
 
     public FoodListElemComponent(Context context, AttributeSet attrs) {
         super(context, attrs);
-        init(attrs, 0);
+        init(context,attrs, 0);
     }
 
     public FoodListElemComponent(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
-        init(attrs, defStyle);
+        init(context,attrs, defStyle);
     }
 
-    private void init(AttributeSet attrs, int defStyle) {
-        // Load attributes
-        final TypedArray a = getContext().obtainStyledAttributes(
-                attrs, R.styleable.FoodListElemComponent, defStyle, 0);
+    private void init(Context context, AttributeSet attrs, int defStyle) {
+        LayoutInflater inflater = (LayoutInflater)
+                context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 
-        mExampleString = "example";
-        mExampleColor = a.getColor(
-                R.styleable.FoodListElemComponent_exampleColor,
-                mExampleColor);
-        // Use getDimensionPixelSize or getDimensionPixelOffset when dealing with
-        // values that should fall on pixel boundaries.
-        mExampleDimension = a.getDimension(
-                R.styleable.FoodListElemComponent_exampleDimension,
-                mExampleDimension);
+        inflater.inflate(R.layout.food_list_comp_temp, this);
 
-        a.recycle();
-
-        mTextPaint = new TextPaint();
-        mTextPaint.setFlags(Paint.ANTI_ALIAS_FLAG);
-        mTextPaint.setTextAlign(Paint.Align.LEFT);
-
-        // Update TextPaint and text measurements from attributes
-        invalidateTextPaintAndMeasurements();
+        countValue = (TextView)findViewById(R.id.countValue);
+        buttonAddValue = (Button) findViewById(R.id.buttonAddValue);
+        buttonDecreaseValue = (Button) findViewById(R.id.buttonDecreaseValue);
     }
 
-    private void invalidateTextPaintAndMeasurements() {
-        mTextPaint.setTextSize(mExampleDimension);
-        mTextPaint.setColor(mExampleColor);
-        mTextWidth = mTextPaint.measureText(mExampleString);
-
-        Paint.FontMetrics fontMetrics = mTextPaint.getFontMetrics();
-        mTextHeight = fontMetrics.bottom;
-    }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-
-        // TODO: consider storing these as member variables to reduce
-        // allocations per draw cycle.
-        int paddingLeft = getPaddingLeft();
-        int paddingTop = getPaddingTop();
-        int paddingRight = getPaddingRight();
-        int paddingBottom = getPaddingBottom();
-
-        int contentWidth = getWidth() - paddingLeft - paddingRight;
-        int contentHeight = getHeight() - paddingTop - paddingBottom;
-
-        // Draw the text.
-        canvas.drawText(mExampleString,
-                paddingLeft + (contentWidth - mTextWidth) / 2,
-                paddingTop + (contentHeight + mTextHeight) / 2,
-                mTextPaint);
     }
 
     /**
@@ -101,26 +57,4 @@ public class FoodListElemComponent extends View {
      *
      * @return The example string attribute value.
      */
-    public String getExampleString() {
-        return mExampleString;
-    }
-    public void setExampleString(String exampleString) {
-        mExampleString = exampleString;
-        invalidateTextPaintAndMeasurements();
-    }
-    public int getExampleColor() {
-        return mExampleColor;
-    }
-    public void setExampleColor(int exampleColor) {
-        mExampleColor = exampleColor;
-        invalidateTextPaintAndMeasurements();
-    }
-    public float getExampleDimension() {
-        return mExampleDimension;
-    }
-
-    public void setExampleDimension(float exampleDimension) {
-        mExampleDimension = exampleDimension;
-        invalidateTextPaintAndMeasurements();
-    }
 }
