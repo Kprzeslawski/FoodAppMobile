a88cbbfef35cd9f7dc9f86a7e103d6202fa9458c
Krzysztof <kprzeslawski01@gmail.com>
2024-05-18 22:21:27 +0200

    example data for records

--- app/src/main/java/com/example/foodappmobile/data/FoodStaticData.java
+++ app/src/main/java/com/example/foodappmobile/data/FoodStaticData.java
@@ -10,5 +10,20 @@ public class FoodStaticData {
         add(new FoodRecordElem(
             "Egg", "Simple egg eat it as u want!", R.drawable.egg
         ));
+        add(new FoodRecordElem(
+                "Milk", "You did what to cow to get that????", R.drawable.mleko
+        ));
+        add(new FoodRecordElem(
+                "Orange Juice", "Perfectly refreshing and sweet", R.drawable.orange_juice
+        ));
+        add(new FoodRecordElem(
+                "Pizza", "Pizza always can fill me up ;)", R.drawable.pizza
+        ));
+        add(new FoodRecordElem(
+                "Uranium-235", "Only 20 000 000kcal in one gram", R.drawable.uran
+        ));
+        add(new FoodRecordElem(
+                "Tiramisu", "Mam ochote na tiramisu xd", R.drawable.tiramisu
+        ));
     }};
 }
